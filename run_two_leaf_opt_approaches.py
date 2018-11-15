6656fb17e3393fd4dfe8b1752a8108c8c44f7496
mdekauwe <mdekauwe@gmail.com>
2018-11-15 13:49:49 +1100

    added matching val

--- run_two_leaf_opt_approaches.py
+++ run_two_leaf_opt_approaches.py
@@ -190,7 +190,8 @@ def main():
             p.max_leaf_width = leaf_width
             p.gamstar25 = 4.36  # 42.75 / 101.25 umol m-2 s-1
             p.Kc25 = 41.0       # 404.9 umol m-2 s-1
-            p.Ko25 = 28202.0    # 278.4 umol m-2 s-1
+            p.Ko25 = 28202.0    # 278.4 mmol mol-1
+            p.O2 = 20.670000    # 210 mmol mol-1
             p.alpha = 0.24
             p.Egamstar= 37830.0
             p.Ec = 79430.0
